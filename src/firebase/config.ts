2e33d335e4a5dfc64578de4b39cc3092441a2527
SK RAZAK <skrazak094@gmail.com>
2026-02-08 15:07:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5505416171-85472",
+  "appId": "1:51029015158:web:1b14e5505da6eb3e38bd57",
+  "apiKey": "AIzaSyBLAtCysOHrkRF207DrL_LWcAMe82R9RE4",
+  "authDomain": "studio-5505416171-85472.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "51029015158"
+};
